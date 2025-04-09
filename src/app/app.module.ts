bfd554d01f8b11122606702ff6fb2fdffe690de8
yassinaha <hayassine0@gmail.com>
2025-04-09 17:07:19 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent{
+
+  titre = 'Hi, I’m Yassine Hanini – A Future Java Developer Building Solutions with a Unique Twist.';
+  bio = 'I’m Yassine Hanini, a passionate learner blending a physics foundation from university with a management diploma into my journey as a Java developer. From analyzing the laws of nature to mastering organizational strategies, I now channel that curiosity and structure into crafting efficient, creative code. I’m driven by problem-solving and excited to build impactful software whether it’s simplifying complex systems or creating tools that make a difference.'
+
+}
